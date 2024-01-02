11b376204ef0f41056f21e7791f62507cae12b83
Jonathan94453 <cino.jonathan@gmail.com>
2024-01-01 22:41:44 -0600

    Added transition effect when mouse enters and exits the sidebar main page must figure out how to incoporate sidebar in rest of elements

--- src/components/SideBar/Sidebar.js
+++ src/components/SideBar/Sidebar.js
@@ -1,14 +1,31 @@
 import "./Sidebar.css" 
 import Logo from "./Logo.png"
 import { Link } from 'react-router-dom';
-import React from 'react';
+import React, {useState} from 'react';
 
-const Sidebar = () => (
-<div className="sidebar-container">
+const Sidebar = () => {
+
+const [issidebarvisible, setsidebarvisible]= useState(false); 
+
+const handleMouseEnter = () => {
+    setsidebarvisible(true); 
+}
+ 
+const handleMouseLeave = () => {
+    setsidebarvisible(false) 
+}
+
+return (
+ 
+<div className="sidebar-container"> 
 
 
+<div
+ className={`sidebar ${issidebarvisible ? 'visible' : ''}`}  
+ onMouseEnter={handleMouseEnter} 
+ onMouseLeave={handleMouseLeave} 
+ >
 
-<div className="sidebar">
 <Link className="logo" to="/"> 
 <img className='img' src={Logo} alt="Logo" />
 </Link> 
@@ -23,16 +40,17 @@ const Sidebar = () => (
 </Link>
 <Link to="/ContactMe">
 <button className="accomplishments">Contact Me</button> 
-</Link>
-
+</Link> 
 </div> 
 {/* test */} 
 
-<div className="maincontent">
+<div 
+className={`maincontent ${issidebarvisible ? 'visible' : ''}`}  
+>
 <div className="heading">
 <h>Hi I'm Jonathan</h>
 <p className="introtext">
-I am 18 years old, and pursuing my Aerospace Engineering degree at Missouri University of Science and Technology in Rolla. <br/>
+I am 18 years old, and pursuing my <br/> Aerospace Engineering degree at Missouri University of Science and Technology in Rolla. <br/>
 
 </p>
 </div>
@@ -42,6 +60,8 @@ I am 18 years old, and pursuing my Aerospace Engineering degree at Missouri Univ
 
 </div>
 
+
 ); 
+}; 
    
 export default Sidebar 
